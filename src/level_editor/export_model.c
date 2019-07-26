5f8183af0c950d3f628caf922cbd5fa755848289
Oscar Pavon <pavon_oscar@outlook.com>
2019-07-26 17:27:52 -0400

    encode data to gltf format

--- src/level_editor/export_model.c
+++ src/level_editor/export_model.c
@@ -8,9 +8,104 @@
 #include "editor.h"
 #include "../third_party/cgltf.h"
 
+#include <ctype.h>
 
 int data_count = 0;
 cgltf_data* data_array[20];
+
+
+
+
+
+static char encoding_table[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                                'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+                                'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+                                'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                                'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                                'w', 'x', 'y', 'z', '0', '1', '2', '3',
+                                '4', '5', '6', '7', '8', '9', '+', '/'};
+static char *decoding_table = NULL;
+static int mod_table[] = {0, 2, 1};
+ 
+ 
+char *base64_encode(const unsigned char *data,
+                    size_t input_length,
+                    size_t *output_length) {
+ 
+    *output_length = 4 * ((input_length + 2) / 3);
+ 
+    char *encoded_data = malloc(*output_length+1);
+    if (encoded_data == NULL) return NULL;
+ 
+    for (int i = 0, j = 0; i < input_length;) {
+ 
+        uint32_t octet_a = i < input_length ? (unsigned char)data[i++] : 0;
+        uint32_t octet_b = i < input_length ? (unsigned char)data[i++] : 0;
+        uint32_t octet_c = i < input_length ? (unsigned char)data[i++] : 0;
+ 
+        uint32_t triple = (octet_a << 0x10) + (octet_b << 0x08) + octet_c;
+ 
+        encoded_data[j++] = encoding_table[(triple >> 3 * 6) & 0x3F];
+        encoded_data[j++] = encoding_table[(triple >> 2 * 6) & 0x3F];
+        encoded_data[j++] = encoding_table[(triple >> 1 * 6) & 0x3F];
+        encoded_data[j++] = encoding_table[(triple >> 0 * 6) & 0x3F];
+    }
+ 
+    for (int i = 0; i < mod_table[input_length % 3]; i++)
+        encoded_data[*output_length - 1 - i] = '=';
+    
+    encoded_data[*output_length] = '\0';
+    return encoded_data;
+}
+ 
+ void build_decoding_table() {
+ 
+    decoding_table = malloc(256);
+ 
+    for (int i = 0; i < 64; i++)
+        decoding_table[(unsigned char) encoding_table[i]] = i;
+}
+
+unsigned char *base64_decode(const char *data,
+                             size_t input_length,
+                             size_t *output_length) {
+ 
+    if (decoding_table == NULL) build_decoding_table();
+ 
+    if (input_length % 4 != 0) return NULL;
+ 
+    *output_length = input_length / 4 * 3;
+    if (data[input_length - 1] == '=') (*output_length)--;
+    if (data[input_length - 2] == '=') (*output_length)--;
+ 
+    unsigned char *decoded_data = malloc(*output_length);
+    if (decoded_data == NULL) return NULL;
+ 
+    for (int i = 0, j = 0; i < input_length;) {
+ 
+        uint32_t sextet_a = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+        uint32_t sextet_b = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+        uint32_t sextet_c = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+        uint32_t sextet_d = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+ 
+        uint32_t triple = (sextet_a << 3 * 6)
+        + (sextet_b << 2 * 6)
+        + (sextet_c << 1 * 6)
+        + (sextet_d << 0 * 6);
+ 
+        if (j < *output_length) decoded_data[j++] = (triple >> 2 * 8) & 0xFF;
+        if (j < *output_length) decoded_data[j++] = (triple >> 1 * 8) & 0xFF;
+        if (j < *output_length) decoded_data[j++] = (triple >> 0 * 8) & 0xFF;
+    }
+ 
+    return decoded_data;
+} 
+
+void base64_cleanup() {
+    free(decoding_table);
+}
+
 
 void load_mesh_for_proccess(const char* path){
     File new_file;
@@ -28,7 +123,7 @@ void load_mesh_for_proccess(const char* path){
     
 }
 
-void export_mesh(ComponentDefinition* component){
+void prepare_mesh_data(ComponentDefinition* component){
     if(component->type == STATIC_MESH_COMPONENT){
         StaticMeshComponent* mesh_component = component->data;
         unsigned int *model_id_in_loaded_models = get_from_array(&mesh_component->meshes,0);
@@ -36,10 +131,109 @@ void export_mesh(ComponentDefinition* component){
         load_mesh_for_proccess(path);
     }
 }
-int export_gltf(const char *name){
-    for_each_element_components(&export_mesh);
 
+void float_to_char(float number,unsigned char* char_number){
+    uint8_t      bytes[sizeof(float)];
+    *(float*)(bytes) = number;  // convert float to bytes
+    char_number[0] = bytes[0];
+    char_number[1] = bytes[1];
+    char_number[2] = bytes[2];
+    char_number[3] = bytes[3];
+}
+
+void uint8_to_char(unsigned short int number, unsigned char* char_number){
+    uint8_t      bytes[sizeof(unsigned short int)];
+    *(unsigned short int*)(bytes) = number;
+    char_number[0] = bytes[0];
+    char_number[1] = bytes[1];
+}
+
+typedef struct Encoded{
+    size_t size;
+    char* data;
+}Encoded;
+
+Encoded vertices_encoded[3];
+int vertices_encoded_count = 0;
+
+void float_to_bytes(float x, float y, float z){
+    uint8_t      bytes[sizeof(float)];
+    *(float*)(bytes) = x;  // convert float to bytes
+    printf("bytes = [ 0x%.2x, 0x%.2x, 0x%.2x, 0x%.2x ]\r\n", bytes[0], bytes[1], bytes[2], bytes[3]);
+
+    unsigned char data[12] = {bytes[0],bytes[1],bytes[2],bytes[3]};
+    printf("Character [ %c , %c , %c , %c ]\n",data[0],data[1],data[2],data[3]);
+
+    *(float*)(bytes) = y;  // convert float to bytes
+    data[4] = bytes[0];
+    data[5] = bytes[1];
+    data[6] = bytes[2];
+    data[7] = bytes[3];
+   
+    *(float*)(bytes) = z;  // convert float to bytes
+    data[8] = bytes[0];
+    data[9] = bytes[1];
+    data[10] = bytes[2];
+    data[11] = bytes[3];
+
+    Encoded encoded_data;
+    encoded_data.size = 0;    
+    encoded_data.data = base64_encode(data,12,&encoded_data.size);
+    printf("Encoded: %s\n",encoded_data.data);
+
+    memcpy(&vertices_encoded[vertices_encoded_count],&encoded_data,sizeof(Encoded));
+    vertices_encoded_count++;
+
+  
+}
+
+int export_gltf(const char *name){
+    for_each_element_components(&prepare_mesh_data);
+
+    cgltf_data* data1 = data_array[0];
+    cgltf_data* data2 = data_array[1];
+
+    cgltf_data new_data;
+    memcpy(&new_data, data1, sizeof(cgltf_data));
+
+    float_to_bytes(0,0,0);
+    float_to_bytes(0,1,0);
+    float_to_bytes(0,0,1);
+ 
     
+
+
+     unsigned char indices_char[3][2];
+
+    uint8_to_char(0,indices_char[0]);
+    uint8_to_char(1,indices_char[1]);
+    uint8_to_char(2,indices_char[2]);
+    Encoded index_encoded[3];
+    for(unsigned short i = 0; i < 3; i++){
+        index_encoded[i].data = base64_encode(&indices_char[i][0],2,&index_encoded[i].size);
+        printf("uint char : %s\n",index_encoded[i].data);
+    }
+    int bytes_count = 0;
+    for(int i = 0; i<3;i++){
+        bytes_count += vertices_encoded[i].size;
+        bytes_count += index_encoded[i].size;
+    }
+
+    printf("Byte size gltf URI: %i\n",bytes_count);
+    
+    char encode[] = {"data:application/octet-stream;base64,"};
+
+    char* new_buffer = malloc(strlen(encode)+bytes_count+1);
+    memset(new_buffer,0,strlen(encode)+bytes_count+1);
+    strcat(new_buffer,encode);
+    for(int i = 0; i<3;i++){
+        strcat(new_buffer,vertices_encoded[i].data);
+    }
+    
+    for(int i = 0; i<3;i++){
+        strcat(new_buffer,index_encoded[i].data);
+    }
+    printf("Uri: %s\n",new_buffer);
 
     cgltf_options options = {0};
     cgltf_data* data_to_export  = data_array[0];
