35763ba2b0068fd267daa8135f936615bde23fdd
Oscar Pavon <pavon_oscar@outlook.com>
2019-08-22 02:42:53 -0400

    Recoverd Blender scripts

---
+++ Source/Scripts/Blender/export.py
@@ -0,0 +1,21 @@
+import bpy
+import sys
+argv = sys.argv
+argv = argv[argv.index("--") + 1:]  # get all args after "--"
+
+print(argv)  # --> ['example', 'args', '123']
+new_file_path = "../generated_glTF_files/"
+new_file_path += argv[0]
+print(new_file_path)
+
+bpy.ops.export_scene.gltf(export_format='GLTF_EMBEDDED', \
+filepath=new_file_path, \
+export_texcoords=True, \
+export_normals=False, export_materials=False, \
+export_selected=True, export_animations=False, \
+export_colors=False, \
+export_skins=False, \
+export_morph=False, \
+export_yup=False
+)
+
