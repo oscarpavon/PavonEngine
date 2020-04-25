4e62675ed9805c5df38a8c6a023c325cc473cbdc
Oscar Pavon <pavon_oscar@outlook.com>
2020-04-25 15:54:47 +0000

    static mesh component now is a source file

--- source/engine/components/components.c
+++ source/engine/components/components.c
@@ -7,6 +7,8 @@
 #include "../../editor/editor.h"
 #endif
 
+#include "static_mesh_component.h"
+
 void add_component_to_selected_element(int component_byte_size, void* new_component, ComponentType type){
     ComponentDefinition new_element_component;
     memset(&new_element_component,0,sizeof(ComponentDefinition));
@@ -112,25 +114,7 @@ void update_component(ComponentDefinition* element_component){
     {
     case STATIC_MESH_COMPONENT:
         {
-            StaticMeshComponent* mesh_component = element_component->data;            
-               
-            for(u8 i = 1; i<=mesh_component->meshes.count-1 ; i++){            
-                u8* id = array_get(&mesh_component->meshes,i);
-                Model* model  = array_get(actual_model_array,*id);
-                glm_mat4_copy(element_component->parent->transform->model_matrix,model->model_mat);                 
-            }
-            
-            u8* id = array_get(&mesh_component->meshes,1);
-            Model* model  = array_get(actual_model_array,*id);
-            if(!model)
-                return;
-            glm_vec3_copy(model->min,mesh_component->bounding_box[0]);
-            glm_vec3_copy(model->max,mesh_component->bounding_box[1]);
-            glm_aabb_transform(mesh_component->bounding_box,
-                                element_component->parent->transform->model_matrix,
-                                mesh_component->bounding_box);
-            glm_aabb_center(mesh_component->bounding_box,mesh_component->center);
-            
+           pe_comp_static_mesh_update(element_component); 
         }
         break;
      case TRASNFORM_COMPONENT:{
@@ -159,46 +143,7 @@ void init_element_component(ComponentDefinition* element_component){
     {
     case STATIC_MESH_COMPONENT:
         {
-            StaticMeshComponent* mesh_component = element_component->data;            
-               
-            for(u32 i = 1; i <= mesh_component->meshes.count-1 ; i++){                
-
-                u8* id = array_get(&mesh_component->meshes,i);
-            
-                Model* original_model = array_get(&array_models_loaded,*id);
-                if(!original_model)
-                    return;
-
-                new_empty_model();
-
-                duplicate_model_data(selected_model,original_model);
-                selected_model->shader = create_engine_shader(standart_vertex_shader,standart_fragment_shader); 
-							
-								u8* texture_id = NULL;
-								if(mesh_component->textures.count > 0)
-									texture_id = array_get(&mesh_component->textures,i);
-                
-								if(texture_id){
-                    Texture* texture = array_get(current_textures_array,*texture_id);
-                    if(texture)
-                        selected_model->texture.id = texture->id;
-                }else{
-                    #ifdef EDITOR
-                    selected_model->texture.id = editor_texture_checker.id;
-                    #endif
-                }
-                glm_mat4_copy(element_component->parent->transform->model_matrix,selected_model->model_mat);               
-                
-            }
-
-            u8 id = actual_model_array->count - ( mesh_component->meshes.count - 1) ;
-            for(u8 i = 1; i <= mesh_component->meshes.count-1 ; i++){
-                u8 * geted_id = array_get(&mesh_component->meshes,i);
-                memcpy(geted_id, &id , sizeof(u8));
-                id++;
-            }
-
-            update_component(element_component);
+					pe_comp_static_mesh_init(element_component);
         }
         break;
      case TRASNFORM_COMPONENT:{
---
+++ source/engine/components/static_mesh_component.h
@@ -0,0 +1,8 @@
+#ifndef COMPONENT_STATIC_MESH
+#define COMPONENT_STATIC_MESH
+#include "components.h"
+
+void pe_comp_static_mesh_init(ComponentDefinition*);
+void pe_comp_static_mesh_update(ComponentDefinition*);
+
+#endif
