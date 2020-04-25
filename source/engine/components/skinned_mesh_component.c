8c94f52e0498bb3ba2199dfdce87bc5c510dbabf
Oscar Pavon <pavon_oscar@outlook.com>
2020-04-25 16:15:53 +0000

    Skinned mesh component now is a source file

---
+++ source/engine/components/skinned_mesh_component.c
@@ -0,0 +1,33 @@
+#include "skinned_mesh_component.h"
+#include <engine/engine.h>
+void pe_comp_skinned_mesh_init(ComponentDefinition* element_component){
+
+  SkinnedMeshComponent *mesh_component = element_component->data;
+  new_empty_model();
+  Model *original_model =
+      array_get(&array_models_loaded, array_models_loaded.count - 1);
+
+  duplicate_model_data(selected_model, original_model);
+  GLuint skin_vertex_shader = load_shader_file(
+      "/home/pavon/PavonEngine/NativeContent/shaders/skin_vertex_shader.glsl",
+      GL_VERTEX_SHADER);
+  selected_model->shader =
+      create_engine_shader(skin_vertex_shader, standart_fragment_shader);
+
+  glm_mat4_copy(element_component->parent->transform->model_matrix,
+                selected_model->model_mat);
+
+  mesh_component->mesh = selected_model;
+
+  memcpy(&mesh_component->joints, &model_nodes, sizeof(Array));
+
+  memcpy(&mesh_component->animations, &model_animation, sizeof(Array));
+
+  memcpy(mesh_component->inverse_bind_matrices,
+         model_loaded_inverse_bind_matrices,
+         sizeof(model_loaded_inverse_bind_matrices));
+
+  mesh_component->node_uniform.joint_count = mesh_component->joints.count;
+
+  update_skeletal_node_uniform();
+}
---
+++ source/engine/components/skinned_mesh_component.h
@@ -0,0 +1,7 @@
+#ifndef PE_COMPONENT_SKINNED
+#define PE_COMPONENT_SKINNED
+#include "components.h"
+
+void pe_comp_skinned_mesh_init(ComponentDefinition*);
+
+#endif
