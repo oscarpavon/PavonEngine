20b990bda347bc463786a35f82ed39bbd19e7d86
Oscar Pavon <pavon_oscar@outlook.com>
2020-08-20 14:02:58 +0000

    fixed triangule with uniform buffers

--- source/engine/renderer/vulkan/uniform_buffer.c
+++ source/engine/renderer/vulkan/uniform_buffer.c
@@ -40,25 +40,26 @@ void pe_vk_uniform_buffer_update(uint32_t image_index){
     vec3 front;
     vec3 position;
 
-    init_vec3(1.0f, 0 ,  0.0f, up);
+    init_vec3(0.0f, 1.0f ,  0.0f, up);
     init_vec3(0.0f, 0.0f,  1.0f, front);
-    init_vec3(0,0,0, position);
+    init_vec3(2.0f, 2.0f, 2.0f, position);
+    
+    glm_rotate(ubo.model,0.002f,VEC3(1,1,0));
 
-    vec3 look_pos;
-    glm_vec3_add(position, front, look_pos);
 
-    glm_lookat(position, look_pos, front , ubo.view);
+    glm_lookat(position, VEC3(0,0,0), up , ubo.view);
 
-    glm_perspective(45.f, camera_width_screen / camera_heigth_screen, 0.001f,
+    glm_perspective(45.f, 1260 / 720, 0.001f,
                     5000.f, ubo.projection);
 
+    ubo.projection[1][1] *= -1;
     
     VkDeviceMemory* memory = array_get(&pe_vk_uniform_buffers_memory,image_index);
 
     void* data;
     vkMapMemory(vk_device, *(memory), 0, sizeof(ubo), 0, &data);
     memcpy(data, &ubo, sizeof(ubo));
-    vkUnmapMemory(vk_device, *memory);
+    vkUnmapMemory(vk_device, *(memory));
   
   
 }
--- source/engine/renderer/vulkan/vulkan.c
+++ source/engine/renderer/vulkan/vulkan.c
@@ -167,20 +167,19 @@ int pe_vk_init() {
   pe_vk_swch_create();
   pe_vk_create_images_views();
 
+  pe_vk_create_descriptor_set_layout();
+  pe_vk_pipeline_init();  
 
   pe_vk_model_create();
 
-  pe_vk_descriptor_pool_create();
-
-  pe_vk_create_descriptor_set_layout();
-
   pe_vk_uniform_buffer_create();
 
-  pe_vk_descriptor_set_create();
+  pe_vk_descriptor_pool_create();
 
-  pe_vk_pipeline_init();  
-
+    
   pe_vk_framebuffer_create();
+
+  pe_vk_descriptor_set_create();
 
   pe_vk_command_init();
   
