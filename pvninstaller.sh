2e3702d18cf0fadaf72be8244a295e8cfe44af52
Oscar Pavon <pavon_oscar@outlook.com>
2022-07-17 20:14:16 -0400

    Shell script installation for android development

---
+++ pvninstaller.sh
@@ -0,0 +1,17 @@
+pkg install neovim
+pkg install make
+pkg install cmake
+pkg install wget
+pkg install tsu
+wget https://dl.google.com/android/repository/android-ndk-r25-linux.zip
+pkg install vulkan-headers
+pkg install binutils
+pkg install mesa
+pkg install freetype
+pkg install aapt
+pkg install dx
+pkg install aapt2
+pkg install ecj
+pkg install automake
+git config --global user.email pavon_oscar@outlook.com
+git config --global user.name Oscar Pavon
